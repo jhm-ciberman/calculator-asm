f30a08a9af6e7132d18b565ce01344a3a30b53e8
Ivan Rodriguez <ivanrodriguez.v@outlook.com>
2019-08-01 19:42:18 -0300

    Adds the Or Command
    DoOr.asm

--- src/logic/commands/DoOr.asm
+++ src/logic/commands/DoOr.asm
@@ -1,4 +1,12 @@
-proc DoOr
+proc DoOr uses r14
+        
+    fastcall ArrayListPop, [_lg_stack]
+    mov r14, rax
+
+    fastcall ArrayListPop, [_lg_stack]
+    or r14, rax
+    
+    fastcall ArrayListPush, [_lg_stack], r14
 
     ret
 endp
