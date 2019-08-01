dbdb55501626df4c9c82c19fc6a82ed758a5f135
Ivan Rodriguez <ivanrodriguez.v@outlook.com>
2019-08-01 20:32:21 -0300

    Adds the / Command
    DoDivide.asm

--- src/logic/commands/DoDivide.asm
+++ src/logic/commands/DoDivide.asm
@@ -1,4 +1,13 @@
-proc DoDivide
+proc DoDivide uses r14
+    ; RAX / R14 --> Stack[N-1]/Stack[N]
+
+    fastcall ArrayListPop, [_lg_stack]
+    mov r14, rax
+
+    fastcall ArrayListPop, [_lg_stack]
+    idiv r14
+    
+    fastcall ArrayListPush, [_lg_stack], rax
 
     ret
 endp
