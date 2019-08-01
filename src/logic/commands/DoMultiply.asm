c8c86cee50930687997c3beed18d7e8c3e560957
Ivan Rodriguez <ivanrodriguez.v@outlook.com>
2019-08-01 19:05:05 -0300

    Adds Multiply Command
    DoMultiply.asm

--- src/logic/commands/DoMultiply.asm
+++ src/logic/commands/DoMultiply.asm
@@ -1,4 +1,12 @@
-proc DoMultiply
+proc DoMultiply uses r14
+        
+    fastcall ArrayListPop, [_lg_stack]
+    mov r14, rax
+
+    fastcall ArrayListPop, [_lg_stack]
+    imul r14, rax
+    
+    fastcall ArrayListPush, [_lg_stack], r14
 
     ret
 endp
