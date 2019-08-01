c67be171c496729631313193708543e92e460aaf
Ivan Rodriguez <ivanrodriguez.v@outlook.com>
2019-08-01 19:43:11 -0300

    Adds the Xor Command
    DoXor.asm

--- src/logic/commands/DoXor.asm
+++ src/logic/commands/DoXor.asm
@@ -1,4 +1,12 @@
-proc DoXor
+proc DoXor uses r14
+        
+    fastcall ArrayListPop, [_lg_stack]
+    mov r14, rax
+
+    fastcall ArrayListPop, [_lg_stack]
+    xor r14, rax
+    
+    fastcall ArrayListPush, [_lg_stack], r14
 
     ret
 endp
